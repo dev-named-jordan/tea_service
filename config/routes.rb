2b9ee8adcfe25a55d6a85d73fe38c250b9810264
Jordan Beck <jordanfbeck@gmail.com>
2021-06-09 12:09:49 -0600

    Removes show from customer_subscriptions routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show] do
-        resources :subscriptions, only: [:index, :show, :update, :create]
+        resources :subscriptions, only: [:index, :update, :create]
       end
     end
   end
