38ba75468d2b1123a87dbe8dfac1aab9f553a0f5
Jordan Beck <jordanfbeck@gmail.com>
2021-06-09 17:37:38 -0600

    Add changes to index action in subscriptions

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
   before_action :create_customer_subscription_params, only: [:create, :update]
 
-  # def index
-    # require "pry"; binding.pry
-    # customer_subscriptions = CustomerSubscription.where(customer_id: "")
-    # render json: customer_subscriptions
-  # end
-
   def create
     subscription = Subscription.where(id: create_customer_subscription_params[:subscription_id].to_i)
 
@@ -15,15 +9,6 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
     customer_subscription = CustomerSubscription.create!(customer_id: create_customer_subscription_params[:customer_id], subscription_id: create_customer_subscription_params[:subscription_id])
     render json: customer_subscription
   end
-
-  # def update
-  #   subscription = Subscription.where(id: create_customer_subscription_params[:subscription_id].to_i)
-  #
-  #   subscription.first.status = "cancelled"
-  #
-  #   # customer_subscription = CustomerSubscription.find_by(customer_id: create_customer_subscription_params[:customer_id], subscription_id:create_customer_subscription_params[:subscription_id])
-  #   render json: subscription
-  # end
 
   private
 
--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,24 +2,13 @@ class Api::V1::SubscriptionsController < ApplicationController
   before_action :update_subscription_params, only: [:update]
 
   def index
-    customer_subscriptions = CustomerSubscription.where(customer_id: params[:customer_id])
+    customer = Customer.find(params[:customer_id])
 
-    subscriptions_1 = Subscription.find_by(id: customer_subscriptions.first[:subscription_id])
-
-    subscriptions_2 = Subscription.find_by(id: customer_subscriptions.last[:subscription_id])
-
-    subscriptions = [subscriptions_1, subscriptions_2]
+    subscriptions = customer.subscriptions
 
     render json: subscriptions
   end
 
-  # def create
-  #   require "pry"; binding.pry
-  #   subscription = Subscription.create!(subscription_params)
-  #
-  #   render json: subscription
-  # end
-  #
   def update
     subscription = Subscription.where(id: params[:id].to_i)
 
